327449910fe6eae41150e790757c38355fcde3cf
RihardsPundurs <rihards.pundurs.2006@gmail.com>
2023-02-19 16:18:36 +0200

    Making player class.

--- classes.py
+++ classes.py
@@ -1,16 +1,40 @@
 import pygame
 
-# class Player(pygame.sprite.Sprite):
-# 	def __init__(self):
-# 	    super().__init__()
-# 	    self.image = pygame.image.load("").convert_alpha()
-# 	    self.image = pygame.transform.scale(self.image, (val1, val2))
-# 	    self.rect = self.image.get_rect(topright=(val1, val2))
+class Player(pygame.sprite.Sprite):
+	def __init__(self):
+	    super().__init__()
+	    self.image = pygame.image.load("").convert_alpha()
+	    self.image = pygame.transform.scale(self.image, (val1, val2))
+	    self.rect = self.image.get_rect(topright=(val1, val2))
+	    self.gravity = 0
+	    self.velocity = 0
 
-# class Block(pygame.sprite.Sprite):
-# 	def __init__(self, cords):
-# 		super().__init__()
-# 		self.cords = cords
-#         self.image = pygame.image.load("").convert_alpha()
-#         self.image = pygame.transform.scale(self.image, (val1, val2))
-#         self.rect = self.image.get_rect(topright=(val1, val2))
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] or keys[pygame.K_W]:
+            self.gravity = -6
+        if keys[pygame.K_A] and self.velocity <= 30:
+        	self.velocity += 1
+        elif keys[pygame.K_A] and self.velocity >= -30:
+        	self.velocity -= 1
+        else:
+        	if self.velocity < 0:
+        		self.velocity += 4
+        	elif self.velocity > 0:
+        		self.velocity -= 4
+
+    def apply_gravity(self):
+        self.gravity += 0.1
+        self.rect.y += self.gravity
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+
+class Block(pygame.sprite.Sprite):
+	def __init__(self, cords):
+		super().__init__()
+		self.cords = cords
+        self.image = pygame.image.load("").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (val1, val2))
+        self.rect = self.image.get_rect(topright=(val1, val2))
--- main.py
+++ main.py
@@ -16,6 +16,16 @@ with open('devlvl.json') as json_file:
   
 print(python_dict.get('Level'))
 
+text_surf = test_font.render("Press ENTER to play", False, "White")
+text_rect = text_surf.get_rect(center=(640, 360))
+
+bg_surf = pygame.image.load("resources/background.png").convert()
+bg_surf = pygame.transform.scale(bg_surf, (1280, 720))
+bg_rect = bg_surf.get_rect(center=(640, 360))
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -27,9 +37,12 @@ while True:
     game_active = True
 
   if game_active:
-  	pass
+    screen.blit(bg_surf, bg_rect)
+	player.draw(screen)
+	player.update()
   else:
-  	pass
+    screen.blit(bg_surf, bg_rect)
+    screen.blit(text_surf, text_rect)
 
   pygame.display.update()
   clock.tick(60)
